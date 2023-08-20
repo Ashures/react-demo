55e135783186dabcb271bcdfb6ff385f6efda0e7
Ashures <ashuresyt@gmail.com>
2023-08-20 18:15:56 +1200

    Added new form with button and input

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,16 @@
+import { Fragment } from "react";
 import "./style.css";
 
 export default function App() {
-  return "Hello, World!";
+  return (
+    <>
+      <form className="add-item-form">
+        <div className="form-row">
+          <label htmlFor="item">Add Item</label>
+          <input type="text" id="item" />
+          <button id="btn">+</button>
+        </div>
+      </form>
+    </>
+  );
 }
